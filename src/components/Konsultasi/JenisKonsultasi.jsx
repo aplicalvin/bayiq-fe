9b5d37803a046bc71414dc015ac75a2d0f1b431f
aplicalvin <thecalvins.sims@gmail.com>
2024-03-31 20:57:27 +0700

    style: add image in Card
    Add image in card :
     - Edukasi/ArtikelCard
     - Konsultasi/JenisKonsultasi
    
    ---

--- src/components/Konsultasi/JenisKonsultasi.jsx
+++ src/components/Konsultasi/JenisKonsultasi.jsx
@@ -1,13 +1,20 @@
+import Image1 from "../../assets/assets/image/image1.jpg";
 function JenisKonsultasi() {
     return (
         <>
-            <div className="w-full lg:w-96 rounded-3xl border border-neutral-300 flex-col justify-start items-start inline-flex bg-stone-50">
-                <div className="h-48 relative bg-rose-50"></div>
+            <div className="w-full lg:w-96 rounded-3xl border border-stone-300 flex-col justify-start items-start inline-flex bg-stone-50">
+                <div className="h-48 relative bg-rose-50">
+                    <img
+                        src={Image1}
+                        className="h-48 relative rounded-t-3xl"
+                        alt=""
+                    />
+                </div>
                 <div className="self-stretch lg:h-32 px-6 pt-3 pb-6 flex-col justify-start items-start flex">
-                    <div className="self-stretch text-neutral-700 text-xl font-bold  leading-7">
+                    <div className="self-stretch text-stone-700 text-xl font-bold  leading-7">
                         Konsultasi Pasutri Baru
                     </div>
-                    <div className="self-stretch text-neutral-400 text-base font-normal  leading-normal">
+                    <div className="self-stretch text-stone-400 text-base font-normal  leading-normal">
                         Sebagai orang tua baru, tentu masih belum punya
                         pengalaman banyak di bidang ini. Jadi jangan langsung
                     </div>
