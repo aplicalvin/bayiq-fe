1959c4ccb900ac90f6ca0f808100153074701f9c
aplicalvin <thecalvins.sims@gmail.com>
2024-04-02 12:01:06 +0700

    style: fix responsive view in HOme/Navbar

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -14,7 +14,7 @@ function HomeBanner() {
                                 Anda orang tua baru yang perlu panduan?
                                 <Link
                                     to="/roadmap"
-                                    className="ml-0 flex items-center text-xs md:text-base font-medium bg-primary-700 text-stone-100 hover:bg-primary-800 md:ml-1 md:inline-flex py-1 px-3 rounded-md"
+                                    className="text-center ml-0 flex items-center text-xs md:text-base font-medium bg-primary-700 text-stone-100 hover:bg-primary-800 md:ml-1 md:inline-flex py-1 px-3 rounded-md"
                                 >
                                     Mulai dari sini
                                 </Link>
