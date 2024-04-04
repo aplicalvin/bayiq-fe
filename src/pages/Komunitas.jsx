e1e607f056696bdfaf4ae42c23eacc22191b4367
aplicalvin <thecalvins.sims@gmail.com>
2024-04-04 22:22:36 +0700

    style: menambah halaman komunitas

--- src/pages/Komunitas.jsx
+++ src/pages/Komunitas.jsx
@@ -1,7 +1,156 @@
+import { Link } from "react-router-dom";
+
 function Komunitas() {
     return (
         <>
-            <h1>Komunitas</h1>
+            <div className="mb-12">
+                <div className="max-w-7xl mx-auto ">
+                    <div className=" w-full px-6  md:px-20 py-12 flex-col justify-start items-center gap-6 inline-flex">
+                        <div className="max-w-2xl w-full  flex-col justify-start items-center gap-3 flex ">
+                            <div className="self-stretch flex-col justify-start items-center gap-1 flex">
+                                <div className="self-stretch text-center text-primary-600 text-lg font-bold leading-7">
+                                    Komunitas
+                                </div>
+                                <div className="self-stretch text-center text-stone-900 text-4xl font-bold leading-10">
+                                    Bergabung bersama komunitas yang saling
+                                    mendukung
+                                </div>
+                            </div>
+                            <div className="self-stretch text-center text-stone-500 text-base font-normal leading-normal">
+                                Lalalalallalalalalalallalalalalalalaal flora
+                                cantik banget aaaaa
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid gap-4 w-full px-4">
+                        <h1 className="text-2xl font-bold text-stone-800">
+                            List Komunitas
+                        </h1>
+                        <div className="grid gap-4 lg:grid-cols-2 ">
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                            <Link
+                                to={
+                                    "https://chat.whatsapp.com/LHvlb2zJG3WBdPovl1Klfi"
+                                }
+                                className="grid gap-2 border-stone-300 rounded-lg bg-neutral-50 border max-w-lg py-2 px-6"
+                            >
+                                <h1 className="font-semibold text-stone-700">
+                                    Komunitas KPPS
+                                </h1>
+                                <p className="font-normal text-xs text-stone-500">
+                                    Komunitas KPPS adalah grup dimana para KPPS
+                                    (Kelompok Panitia Penyelenggara Pemilihan
+                                    Suara) berkumpul
+                                </p>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
