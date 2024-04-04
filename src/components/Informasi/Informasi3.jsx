ffc61959f5a08f7979309a512e12d915d7b3a54a
aplicalvin <thecalvins.sims@gmail.com>
2024-04-04 11:18:21 +0700

    style:add responsif in informasi

--- src/components/Informasi/Informasi3.jsx
+++ src/components/Informasi/Informasi3.jsx
@@ -4,11 +4,11 @@ function Informasi3() {
     return (
         <>
             <div className="flex justify-center w-full h-fit  place-self-center">
-                <div className="max-w-[1440px] w-fit lg:w-full px-6 md:px-20 py-12 justify-between grid gap-6">
+                <div className="max-w-[1440px] w-fit md:w-full px-6 md:px-20 py-12 justify-between grid gap-6">
                     <h1 className="font-bold text-xl text-stone-800">
                         Bidan terdekat dari rumah Anda
                     </h1>
-                    <div className="self-stretch justify-start items-start gap-6 grid md:grid-cols-2 lg:grid-cols-4">
+                    <div className="self-stretch justify-start items-start gap-6 grid md:grid-cols-3 lg:grid-cols-4">
                         <CardBidan />
                         <CardBidan />
                         <CardBidan />
