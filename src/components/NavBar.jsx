96c4f70ee5ece2456801c770ea8e6fcca3bfb07a
aplicalvin <thecalvins.sims@gmail.com>
2024-04-05 23:50:56 +0700

    style:refactor color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/assets/logo.png";
 
 function NavBar() {
     return (
-        <Navbar fluid rounded className="bg-primary-50 h-fit lg:px-20">
+        <Navbar fluid rounded className="bg-primary-200 h-fit lg:px-20">
             <Navbar.Brand href="/">
                 <img src={logo} className="mr-3 h-6 sm:h-12" alt="Logo" />
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white"></span>
